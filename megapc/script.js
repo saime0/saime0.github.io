e19b2ec7b59b337e92b6f4e0ccf781edadbdd0f0
saime0 <67008522+saime0@users.noreply.github.com>
2021-06-10 20:28:42 +0300

    1

--- megapc/script.js
+++ megapc/script.js
@@ -2,10 +2,8 @@ let films = [
     {   
         name: 'HYPERPC AMPERE', 
         duration: "850 000грн", 
-         
         actors:"Іван, Степан, Павло, Катя", 
-        image:"7.jpg",
-        
+        image:"ampere.png",
         about:"Комплектующие",
         videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
         processor:"AMD Ryzen Threadripper 3990X",
@@ -13,105 +11,143 @@ let films = [
         opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
        
     },
-    {   
-        name: 'Паддінгтон', 
-        duration: 108, 
-         
+    {
+        name: 'HYPERPC DYNAMIC', 
+        duration: "270 000", 
+
         actors:"Іван, Степан, Павло, Катя", 
-        image:"8.jpg",
-       
-        about:"2",
-        videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
-        processor:"AMD Ryzen Threadripper 3990X",
-        matplat:"ASUS ROG ZENITH II EXTREME ALPHA",
-        opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
-    },
-    {   
-        name: 'Зелена книга', 
-        duration: 108, 
-       
-        actors:"Іван, Степан, Павло, Катя", 
-        image:"9.jpg",
-        
-        about:"3",
-        videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
-        processor:"AMD Ryzen Threadripper 3990X",
-        matplat:"ASUS ROG ZENITH II EXTREME ALPHA",
-        opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
-    },
-    {   
-        name: '1+1. Недоторкані', 
-        duration: 108, 
-         
-        actors:"Іван, Степан, Павло, Катя", 
-        image:"10.jpg",
+        image:"dynamic.png",
         about:"1",
-        videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
-        processor:"AMD Ryzen Threadripper 3990X",
-        matplat:"ASUS ROG ZENITH II EXTREME ALPHA",
-        opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
+        videocard:"Palit GeForce RTX 3080 Ti Gaming Pro",
+        processor:"Intel® Core™ i9-11900K(F)",
+        matplat:"ASUS ROG MAXIMUS XIII HERO",
+        opera:"4 x 16GB HyperX Predator RGB DDR4-3200",
     },
-    {   
-        name: '1+1. Недоторкані', 
-        duration: 108, 
-       
+    {
+        name: 'HYPERPC CONCEPT 4',
+        duration: "810 000 грн" , 
         actors:"Іван, Степан, Павло, Катя", 
-        image:"11.jpg",
-        about:"1",
+        image:"concept.png",
+        about:"Комплектующие",
         videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
         processor:"AMD Ryzen Threadripper 3990X",
         matplat:"ASUS ROG ZENITH II EXTREME ALPHA",
-        opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
+        opera:" 8 x 16GB G.Skill Trident Z Royal DDR4-4000",
     },
-    {   
-        name: '1+1. Недоторкані', 
-        duration: 108, 
-       
+    
+    {
+        name: 'HYPERPC CYBER PLATINUM', 
+        duration: "330 000", 
+
         actors:"Іван, Степан, Павло, Катя", 
-        image:"12.jpg",
+        image:"cyber.png",
         about:"1",
-        videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
-        processor:"AMD Ryzen Threadripper 3990X",
-        matplat:"ASUS ROG ZENITH II EXTREME ALPHA",
-        opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
+        videocard:"ASUS GeForce RTX 3090 STRIX",
+        processor:"Intel® Core™ i9-11900K(F)",
+        matplat:"ASUS ROG MAXIMUS XIII HERO",
+        opera:"4 x 16GB Corsair Dominator Platinum RGB DDR4 3000",
     },
-    {   
-        name: '1+1. Недоторкані', 
-        duration: 108, 
-         
+    {
+        name: 'HYPERPC VOLT V2', 
+        duration: "450 000 грн", 
+
         actors:"Іван, Степан, Павло, Катя", 
-        image:"10.jpg",
+        image:"volt.png",
         about:"1",
-        videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
-        processor:"AMD Ryzen Threadripper 3990X",
-        matplat:"ASUS ROG ZENITH II EXTREME ALPHA",
-        opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
+        videocard:"MSI GeForce RTX 3080 Ti GAMING X TRIO",
+        processor:"Intel® Core™ i9-11900K(F)",
+        matplat:"ASUS ROG MAXIMUS XIII HERO",
+        opera:"4 x 16GB HyperX Predator RGB DDR4-3200",
     },
-    {   
-        name: '1+1. Недоторкані', 
-        duration: 108, 
-        
+   { name: "HYPERPC INVADER", 
+        duration: "215 000 грн", 
+
         actors:"Іван, Степан, Павло, Катя", 
-        image:"10.jpg",
+        image:"invader.png",
         about:"1",
-        videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
-        processor:"AMD Ryzen Threadripper 3990X",
-        matplat:"ASUS ROG ZENITH II EXTREME ALPHA",
-        opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
+        videocard:"ASUS GeForce RTX 3080 Ti TUF",
+        processor:"Intel® Core™ i9-11900K(F)",
+        matplat:"ASUS ROG MAXIMUS XIII HERO",
+        opera:"2 x 16GB HyperX Predator RGB DDR4-3200",
     },
-    {   
-        name: '1+1. Недоторкані', 
-        duration: 108, 
-      
+    {
+        name: 'HYPERPC DESK PLUS', 
+        duration:  "750 000 грн", 
         actors:"Іван, Степан, Павло, Катя", 
-        image:"10.jpg",
-        about:"1",
+        image:"desk.png",
+        about:"Комплектующие",
         videocard:"2 x ASUS GeForce RTX 3090 TUF Gaming",
-        processor:"AMD Ryzen Threadripper 3990X",
-        matplat:"ASUS ROG ZENITH II EXTREME ALPHA",
-        opera:"8 x 16GB G.Skill Trident Z Royal DDR4-4000",
+        processor:"  2 x AMD Ryzen 9 5950X",
+        matplat:"  2 x ASUS ROG CROSSHAIR VIII DARK HERO",
+        opera:" 8 x 16GB HyperX Predator RGB DDR4-3600",
+
+    },
+    
+    
+    {
+        name: 'HYPERPC PLAY v2', 
+        duration: "95 000", 
+
+        actors:"Іван, Степан, Павло, Катя", 
+        image:"play.png",
+        about:"1",
+        videocard:"NVIDIA GeForce RTX 3080 для ноутбуков",
+        processor:"AMD Ryzen 7 5800H",
+        display:"15.6, 2560 x 1440px, 165 Гц, IPS, 3мс",
+        opera:"2 x 16GB HyperX Impact DDR4-3200",
+    },
+
+ {
+        name: 'MSI Creator 17 Extrem', 
+        duration: "120 000", 
+
+        actors:"Іван, Степан, Павло, Катя", 
+        image:"msi1.png",
+        about:"1",
+        videocard:"NVIDIA GeForce RTX 2080 Super Max-Q",
+        processor:" Intel Core i7-10875H",
+        display:"17.3  3840х2160",
+        opera:"32GB DDR4",
+    },
+{
+        name: 'MSI GS66 Stealth Optimal', 
+        duration: "110 000", 
+
+        actors:"Іван, Степан, Павло, Катя", 
+        image:"msi2.png",
+        about:"1",
+        videocard:" NVIDIA GeForce RTX 3080",
+        processor:" Intel Core i7-10875H",
+        display:"15.6  1920х1080",
+        opera:"32GB DDR4",
+    },
+{
+        name: 'Acer Predator Helios 300 ', 
+        duration: "100 000", 
+
+        actors:"Іван, Степан, Павло, Катя", 
+        image:"predator300.png",
+        about:"1",
+        videocard:" NVIDIA GeForce RTX 3080",
+        processor:" Intel Core i7-10875H",
+        display:"15.6 (1920x1080) Full HD 300 Гц",
+        opera:"32GB DDR4",
+    },
+
+{
+        name: 'Acer Predator Helios 700 ', 
+        duration: "100 000", 
+
+        actors:"Іван, Степан, Павло, Катя", 
+        image:"predator700.png",
+        about:"1",
+        videocard:" NVIDIA GeForce RTX 2070 Super",
+        processor:" Intel Core i7-10875H",
+        display:"17.3'' (1920x1080) Full HD 144 Гц",
+        opera:"32GB DDR4",
     },
 ];
+
 
 let app = {
     data() {
@@ -167,5 +203,25 @@ let app = {
 
 }
 
+
+let formRegister = document.querySelector('#window');
+let button = document.querySelector('.regButton');
+let divClose = document.querySelector('.close');
+
+let shadow = document.createElement('div');
+shadow.id = 'shadow';
+
+
+function open(){
+    document.body.appendChild(shadow);
+    formRegister.style.opacity = 1;
+}
+function close(){
+    shadow.parentNode.removeChild(shadow);
+    formRegister.style.opacity = 0;
+}
+button.onclick = open;
+divClose.onclick = close;
+shadow.onclick = close;
 Vue.createApp(app).mount("#film-app")
 
